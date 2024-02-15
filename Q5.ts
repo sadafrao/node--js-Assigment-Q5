7dffb0190cc98ea3e25eb6fbf0224c30b937311e
sadafrao <159582784+sadafrao@users.noreply.github.com>
2024-02-14 21:18:31 -0800

    Add files via upload

---
+++ Q5.ts
@@ -0,0 +1,9 @@
+// Q5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famous_person: string = "Hazrat ALI"
+
+let message: string = " Accept the appology, Even if it is not sincer "
+
+
+// console.log(`“${famous_person} once said,${message}”`)
+
+console.log(`${famous_person} once said,“${message}”`)
